6d567e11762618924e7ba4de8179e5ae9ae4d497
HomeArchbishop <1984096954@qq.com>
2023-08-14 22:32:30 +0800

    fix(server-base): reqs order & async streams cb

--- server/index.ts
+++ server/index.ts
@@ -14,29 +14,40 @@ const router = new Router()
 
 const msgHandler = new MsgHandler()
 
-router.post('/', async (ctx, next) => {
+const msgQueue = {
+  cur: Promise.resolve(),
+  async add (promise: Promise<void>) {
+    msgQueue.cur = msgQueue.cur.then(async () => await promise)
+    await msgQueue.cur
+  }
+}
+
+router.post('/', async function (ctx, next) {
   const params: Buffer[] = []
-  ctx.req.on('data', (chunk: Buffer) => {
-    params.push(chunk)
-  })
-  ctx.req.on('end', () => {
-    const buffer = Buffer.concat(params)
-    const msgType = ctx.query.msg as 'req' | 'res'
-    const canvasW = +(ctx.query.w ?? 0)
-    const canvasH = +(ctx.query.h ?? 0)
-    const canvasScreenX = +(ctx.query.x ?? 0)
-    const canvasScreenY = +(ctx.query.y ?? 0)
-    const isWindowFocus = ctx.query.f === 'true'
-    const dpi = +(ctx.query.dpi ?? 1)
-    if (msgType === 'res') {
-      logger.info('<server-base> Server received res buffer: ' + JSON.stringify(buffer.toJSON().data))
-      msgHandler.handleRes(buffer, ctx.query.meID as string | undefined, isWindowFocus ? { bot, canvasW, canvasH, canvasScreenX, canvasScreenY, dpi } : undefined)
-    } else if (msgType === 'req') {
-      logger.info('<server-base> Server received req buffer')
-      msgHandler.handleReq(buffer)
-    }
-  })
-  ctx.body = { code: 200 }
+  await msgQueue.add(new Promise<void>((resolve, reject) => {
+    ctx.req.on('data', (chunk: Buffer) => {
+      params.push(chunk)
+    })
+    ctx.req.on('end', () => {
+      const buffer = Buffer.concat(params)
+      const msgType = ctx.query.msg as 'req' | 'res'
+      const canvasW = +(ctx.query.w ?? 0)
+      const canvasH = +(ctx.query.h ?? 0)
+      const canvasScreenX = +(ctx.query.x ?? 0)
+      const canvasScreenY = +(ctx.query.y ?? 0)
+      const isWindowFocus = ctx.query.f === 'true'
+      const dpi = +(ctx.query.dpi ?? 1)
+      if (msgType === 'res') {
+        logger.info('<server-base> Server received res buffer: ' + JSON.stringify(buffer.toJSON().data))
+        msgHandler.handleRes(buffer, ctx.query.meID as string | undefined, isWindowFocus ? { bot, canvasW, canvasH, canvasScreenX, canvasScreenY, dpi } : undefined)
+      } else if (msgType === 'req') {
+        logger.info('<server-base> Server received req buffer')
+        msgHandler.handleReq(buffer)
+      }
+      ctx.body = { code: 200 }
+      resolve()
+    })
+  }))
   await next()
 })
 
