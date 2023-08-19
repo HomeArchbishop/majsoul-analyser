d13dff0ae373f771ef7ef8612fa0797f085b81be
HomeArchbishop <1984096954@qq.com>
2023-08-19 14:01:47 +0800

    fix(utils): dora `nextTile()` turn 6z to 0z

--- server/utils/nextTile.ts
+++ server/utils/nextTile.ts
@@ -2,9 +2,9 @@ import { Tile } from '../types/General'
 
 function nextTile (tile: Tile): Tile {
   if (tile.endsWith('z')) {
-    return String((+tile[0] + 1) % 7) + tile[1] as Tile
+    return String((+tile[0] % 7) + 1) + tile[1] as Tile
   } else {
-    return String((+tile[0] + 1) % 9) + tile[1] as Tile
+    return String((+tile[0] % 9) + 1) + tile[1] as Tile
   }
 }
 
